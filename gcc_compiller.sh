cac12b0a8ad54f39d6d3e0bacf1d319818cba457
dxb3303 <sonnydeco2020@gmail.com>
2025-08-20 16:51:27 +0500

    изменен скрипт gcc_compiler.sh

--- gcc_compiller.sh
+++ gcc_compiller.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # Для компиляции файла по требуемым стандартам
 gcc -std=c11 -Wall -Werror -Wextra $1
+
+# Форматирование кода по стандарту
+clang-format -i $1
+
+# Проверка на memory leak
+cppcheck --enable=all --suppress=missingIncludeSystem $1
