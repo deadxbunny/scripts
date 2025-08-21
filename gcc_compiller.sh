3b5c46f3c10fd486728d2475a471cb9782bc1795
dxb3303 <sonnydeco2020@gmail.com>
2025-08-21 11:51:19 +0500

    .

--- gcc_compiller.sh
+++ gcc_compiller.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# Для компиляции файла по требуемым стандартам
-gcc -std=c11 -Wall -Werror -Wextra $1
 
 # Форматирование кода по стандарту
 clang-format -i $1
 
 # Проверка на memory leak
 cppcheck --enable=all --suppress=missingIncludeSystem $1
+
+# Для компиляции файла по требуемым стандартам
+gcc -std=c11 -Wall -Werror -Wextra $1
